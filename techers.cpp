d7c1fdd4afa964b8bb33c2bf6bd4662b8f22a063
Satyam-Private <satyampatil1252@gmail.com>
2024-12-18 11:17:19 +0530

    real world example

---
+++ techers.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class teachers{
+    public:
+    string name;
+    string dept;
+    string subject;
+    double salary;
+    void Name(string newName){
+        name = newName;
+        cout<<"your name is : "<<newName<<endl;
+    }
+    void changeDept(string newDept){
+        dept = newDept;
+        cout<<"your new department is : "<<dept<<endl;
+    }
+    void getInfo(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Department : "<<dept;
+    }
+
+};
+
+
+int main(){
+    teachers t1;
+    string name , dept;
+    cout<<"enter your name : ";
+    cin>>name;
+    t1.Name(name);
+    cout<<"enter your new department : ";
+    cin>>dept;
+    t1.changeDept(dept);
+    t1.getInfo();
+    return 0;
+}
