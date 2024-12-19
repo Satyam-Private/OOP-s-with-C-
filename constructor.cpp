fc615206c9f4d97ecbaf46b7c785857a568ba05c
Satyam-Private <satyampatil1252@gmail.com>
2024-12-19 11:57:15 +0530

    Implemented constructor in oops using cpp

---
+++ constructor.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+// constructor is the method in class that has same name as class name 
+// constructor doesnt have any return type 
+class home{
+    public:
+    int size , rooms , floors;
+    home(){
+        cout<<"this is inside the constructor function "<<endl;
+        cout<<"enter the infornmation of your home : ";
+        cout<<endl<<"enter the size of the house : ";
+        cin>>size;
+        cout<<endl<<"enter the number of rooms in house : ";
+        cin>>rooms;
+        cout<<endl<<"enter the number of floors in house : ";
+        cin>>floors;
+    }
+    void info(){
+        cout<<"House information : ";
+        cout<<endl<<"size of the house : "<<size;
+        cout<<endl<<"Number of rooms : "<<rooms;
+        cout<<endl<<"Number of floors : "<<floors;
+    }
+    int priceCal(){
+        cout<<"the price of the room is : ";
+        int total_price = 2000 * size + 2000 * rooms + 10000 * floors;
+        return total_price;
+    }
+    
+};
+
+int main(){
+    // for first object 
+    home myHome1;
+    myHome1.info();
+    int price1 = myHome1.priceCal();
+    cout<<endl<<"the total price of home is : "<<price1<<endl;
+    // for seconde object 
+    home myHome2;
+    myHome2.info();
+    int price2 = myHome2.priceCal();
+    cout<<endl<<"the total price of home is : "<<price2;
+    return 0;
+}
